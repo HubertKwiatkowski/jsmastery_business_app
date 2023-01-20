f8a1a42ae1e1e3f771214f9868a771f27c4bc1e8
Hubert <h.kwiatkowski@hotmail.com>
2023-01-20 12:40:19 +0100

    billing section done

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const App = () => (
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
+        <Billing />
         <CardDeal />
         <Testimonials />
         <Clients />
